9bb33956de38ce7fb4fc91a200f7d0f7f962a0d3
Ishu Jain <ishu.jain@cognizant.com>
2018-12-11 12:15:43 +0530

    ui initial

--- ui/tshell/src/app/app-routing.module.ts
+++ ui/tshell/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssessmenthistoryComponent } from './assessmenthistory/assessmenthistory.component';
 
 const routes: Routes = [
-  {}
+   {path:"",component:AssessmenthistoryComponent}
 ];
 
 @NgModule({
--- ui/tshell/src/app/app.module.ts
+++ ui/tshell/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssessmenthistoryComponent } from './assessmenthistory/assessmenthistory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AssessmenthistoryComponent,
     
   ],
   imports: [
