caa9087751a44c946e8a81c05ca99fea6a4de54e
Vamsi Krishna <vamsi.kaja@cognizant.com>
2018-12-11 10:15:43 +0530

    resolved errors

--- service/tShell-webservice/src/main/java/com/cts/tshell/repository/QuestionDifficultyRepository.java
+++ service/tShell-webservice/src/main/java/com/cts/tshell/repository/QuestionDifficultyRepository.java
@@ -2,6 +2,8 @@ package com.cts.tshell.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionDifficultyRepository extends JpaRepository<Integer, Integer>{
+import com.cts.tshell.bean.QuestionDifficulty;
+
+public interface QuestionDifficultyRepository extends JpaRepository<QuestionDifficulty, Integer>{
 
 }
