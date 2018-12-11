28ae5ba95ee12fff7b3e89f91b0313fcb9ffb004
Aastha Teji <aastha.teji@cognizant.com>
2018-12-11 16:07:06 +0530

    validations added in signup

--- ui/tshell/src/app/app.module.ts
+++ ui/tshell/src/app/app.module.ts
@@ -3,15 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import {  HttpClientModule } from '../../node_modules/@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
+import { ConfirmEqualValidatorDirective } from './signup/confirm-equal-validator-directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
+    ConfirmEqualValidatorDirective
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
