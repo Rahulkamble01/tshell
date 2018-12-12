4fdd7eef3a12eb9e6ef6f6f3f22c9e539d15d3ac
Ishu Jain <ishu.jain@cognizant.com>
2018-12-12 15:03:52 +0530

    deactivate skill done

--- ui/tshell/src/app/app-routing.module.ts
+++ ui/tshell/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchSkillComponent } from './search-skill/search-skill.component';
 
 const routes: Routes = [
-  {}
+  {path: '' , component:  SearchSkillComponent },
 ];
 
 @NgModule({
--- ui/tshell/src/app/app.module.ts
+++ ui/tshell/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchSkillComponent } from './search-skill/search-skill.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchSkillComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
