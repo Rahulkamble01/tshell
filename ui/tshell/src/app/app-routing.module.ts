93193c74241779dd819e9c181d17ada6f8a17157
Akrit kukreti <akrit.kukreti@cognizant.com>
2018-12-12 11:46:14 +0530

    Graph functionality added.

--- ui/tshell/src/app/app-routing.module.ts
+++ ui/tshell/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MostSearchedSkillsComponent } from './most-searched-skills/most-searched-skills.component';
+
 
 const routes: Routes = [
-  {}
+  {path:'', component:DashboardComponent}
 ];
 
 @NgModule({
--- ui/tshell/src/app/app.module.ts
+++ ui/tshell/src/app/app.module.ts
@@ -4,14 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MostSearchedSkillsComponent } from './most-searched-skills/most-searched-skills.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    MostSearchedSkillsComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
