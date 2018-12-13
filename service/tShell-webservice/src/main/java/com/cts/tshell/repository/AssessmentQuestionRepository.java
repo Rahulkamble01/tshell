a3f7166ecb0fd56ecf1d2eddd4257b3066c60d20
Vinaykumar Mandava <vinaykumar.mandava@cognizant.com>
2018-12-13 15:43:43 +0530

    before merging

--- service/tShell-webservice/src/main/java/com/cts/tshell/repository/AssessmentQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.tshell.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.tshell.bean.AssessmentQuestion;
-
-public interface AssessmentQuestionRepository extends JpaRepository<AssessmentQuestion, Integer> {
-
-}
--- service/tShell-webservice/src/main/java/com/cts/tshell/repository/QuestionDifficultyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cts.tshell.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionDifficultyRepository extends JpaRepository<Integer, Integer>{
-
-}
