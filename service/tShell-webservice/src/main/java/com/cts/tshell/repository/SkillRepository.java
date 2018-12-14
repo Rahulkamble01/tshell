51215e117e4bde662f9d92a07fd2015fc50b7dd3
Sangamesh Salimath <sangamesh.salimath@cognizant.com>
2018-12-14 17:48:09 +0530

    deleted database and service folder from the branch

--- service/tShell-webservice/src/main/java/com/cts/tshell/repository/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.tshell.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.tshell.bean.Skill;
-
-public interface SkillRepository extends JpaRepository<Skill, Integer>{
-
-}
