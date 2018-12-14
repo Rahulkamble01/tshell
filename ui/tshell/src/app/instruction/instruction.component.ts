295d30bfcad64b3c293b97e1502a8e3e8ed5f284
Kiran Ravariya <kiran.ravariya@cognizant.com>
2018-12-14 17:04:59 +0530

    score instructions and assesment added and wqorking

--- ui/tshell/src/app/instruction/instruction.component.ts
+++ ui/tshell/src/app/instruction/instruction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-instruction',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstructionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  startAssesment(){
+  this.router.navigate(['/assesment']);
+}
 }
