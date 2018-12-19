7c40001eeea86877e6b438d00c4959c01280a36e
Kiran Ravariya <kiran.ravariya@cognizant.com>
2018-12-19 18:21:21 +0530

    Eclipse Service  Project Added

---
+++ service/tshell/src/main/java/com/cts/tshell/bean/AssessmentQuestion.java
@@ -0,0 +1,63 @@
+package com.cts.tshell.bean;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="assessment_question")
+public class AssessmentQuestion {	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="aq_id")
+	private int id;
+	
+	@Column(name="aq_as_id")
+	private Assessment assessment;
+	
+	@Column(name="aq_qu_id")
+	private Question question;
+	
+	@OneToMany(fetch=FetchType.LAZY,mappedBy="assessmentQuestion")
+	private List<AssessmentQuestionOption> assessmentQuestionOption;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Assessment getAssessment() {
+		return assessment;
+	}
+
+	public void setAssessment(Assessment assessment) {
+		this.assessment = assessment;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public List<AssessmentQuestionOption> getAssessmentQuestionOption() {
+		return assessmentQuestionOption;
+	}
+
+	public void setAssessmentQuestionOption(List<AssessmentQuestionOption> assessmentQuestionOption) {
+		this.assessmentQuestionOption = assessmentQuestionOption;
+	}
+}
---
+++ service/tshell/src/main/java/com/cts/tshell/bean/AssessmentQuestionOption.java
@@ -0,0 +1,67 @@
+package com.cts.tshell.bean;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="assessment_question_option")
+public class AssessmentQuestionOption {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ao_id")
+	private int id;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@Column(name="ao_aq_id")
+	private AssessmentQuestion assessmentQuestion;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@Column(name="ao_op_id")
+	private Option assessmentOption;
+	
+	@Column(name="ao_is_selected")
+	private boolean selected;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public AssessmentQuestion getAssessmentQuestion() {
+		return assessmentQuestion;
+	}
+
+	public void setAssessmentQuestion(AssessmentQuestion assessmentQuestion) {
+		this.assessmentQuestion = assessmentQuestion;
+	}
+
+	public Option getAssessmentOption() {
+		return assessmentOption;
+	}
+
+	public void setAssessmentOption(Option assessmentOption) {
+		this.assessmentOption = assessmentOption;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+}
