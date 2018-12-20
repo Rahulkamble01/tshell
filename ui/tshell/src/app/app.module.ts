3bd376a35806c2e1a1fcbc3e10a66c98debee699
Rachuri Sudheer <rachuri.sudheer@cognizant.com>
2018-12-20 12:02:08 +0530

    app.module updated

--- ui/tshell/src/app/app.module.ts
+++ ui/tshell/src/app/app.module.ts
@@ -61,7 +61,7 @@ PasswordNotEqualValidatorDirective,
     DashboardComponent,
     MostSearchedSkillsComponent,
     TotalquestionComponent,
-    RecentlyAddedSkillsComponent,
+    
     LoginComponent,
     AdminSignupComponent,
     SignupComponent,
