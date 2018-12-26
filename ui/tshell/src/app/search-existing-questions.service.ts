9df7525ce403cdbe0ca89f7c4c848db939687f6a
Vishal Sharma <vishal.sharma7@cognizant.com>
2018-12-26 11:55:49 +0530

    add option based on question is done

--- ui/tshell/src/app/search-existing-questions.service.ts
+++ ui/tshell/src/app/search-existing-questions.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions={
+  headers: new HttpHeaders({
+    'Content-Type':'application/json',
+  })
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +21,19 @@ export class SearchExistingQuestionsService {
     let urlSearchQuestions: string = '/questions/findExistingQuestions/' + searchQuery;
     console.log(urlSearchQuestions);
     return this.http.get<any>(urlSearchQuestions);
-  }
 }
+
+
+     urlOption :string = "/tShell/question/option/add ";
+
+      addOption(option1):Observable<any>{
+       console.log("inside add option");
+     console.log(option1);
+     return this.http.post<any>(this.urlOption, option1, httpOptions);
+}
+
+ 
+
+}
+
+
--- ui/tshell/src/app/search-existing-questions/search-existing-questions.component.ts
+++ ui/tshell/src/app/search-existing-questions/search-existing-questions.component.ts
@@ -9,12 +9,13 @@ import { FormBuilder } from '@angular/forms';
 })
 export class SearchExistingQuestionsComponent implements OnInit {
 
-  constructor(private service:SearchExistingQuestionsService, private fb:FormBuilder) { }
+  constructor(private service: SearchExistingQuestionsService, private fb: FormBuilder) { }
 
   ngOnInit() {
+
   }
 
-  questions:string="Which of the following are legal lines of Java code?, Which data type value is returned by all transcendental math functions?Which of these literals can be contained in float data type variable? What is the range of short data type in Java?";
+  /* questions:string="Which of the following are legal lines of Java code?, Which data type value is returned by all transcendental math functions?Which of these literals can be contained in float data type variable? What is the range of short data type in Java?";
   option1:string="int w = (int)888.8; byte x = (byte)100L;long y = (byte)100;byte z = (byte)100L;";
   count=0;
   questionList:any;
@@ -40,5 +41,36 @@ export class SearchExistingQuestionsComponent implements OnInit {
       }
     )
   }
+ */
+  option: string;
+  description: string = "description";
+
+
+
+  option1 = {
+    answer: true,
+    question: {
+      id: 2,
+    }
+  };
+
+
+  addOption() {
+    
+    console.log('inside addOption()');
+    console.log(JSON.stringify(this.option1));
+    this.option1[this.description] = this.option;
+    
+    console.log(JSON.stringify(this.option1));
+    this.service.addOption(JSON.stringify(this.option1)).subscribe(data => {
+    console.log(data);
+
+    }
+  )
+
+  }
+
+
 
 }
+
