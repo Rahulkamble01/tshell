32a3dfa7eac16356e9eaa0e7383cbc134e9133fc
Rachuri Sudheer <rachuri.sudheer@cognizant.com>
2018-12-27 11:11:00 +0530

    app module changed

--- ui/tshell/src/app/app.module.ts
+++ ui/tshell/src/app/app.module.ts
@@ -60,7 +60,7 @@ PasswordNotEqualValidatorDirective,
     SkillmodalComponent,
     DashboardComponent,
     MostSearchedSkillsComponent,
-    TotalquestionComponent,
+    
     
     RecentlyAddedSkillsComponent,
     LoginComponent,
