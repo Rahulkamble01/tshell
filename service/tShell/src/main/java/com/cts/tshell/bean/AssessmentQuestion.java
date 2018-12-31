c8b4fc2a01e0253139b92e714503e2f6b5bb19ad
Kaja <vamsi.kaja@cognizant.com>
2018-12-31 15:51:26 +0530

    Resolved issues in service

---
+++ service/tShell/src/main/java/com/cts/tshell/bean/AssessmentQuestion.java
@@ -0,0 +1,72 @@
+package com.cts.tshell.bean;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@Table(name="assessment_question")
+@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
+public class AssessmentQuestion {	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="aq_id")
+	private int id;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="aq_as_id")
+	private Assessment assessment;
+	
+	@ManyToOne(fetch=FetchType.LAZY,cascade=CascadeType.ALL)
+	@JoinColumn(name="aq_qu_id")
+	private Question question;
+	
+	@OneToMany(fetch=FetchType.LAZY,mappedBy="assessmentQuestion")
+	private List<AssessmentQuestionOption> assessmentQuestionOption;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Assessment getAssessment() {
+		return assessment;
+	}
+
+	public void setAssessment(Assessment assessment) {
+		this.assessment = assessment;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public List<AssessmentQuestionOption> getAssessmentQuestionOption() {
+		return assessmentQuestionOption;
+	}
+
+	public void setAssessmentQuestionOption(List<AssessmentQuestionOption> assessmentQuestionOption) {
+		this.assessmentQuestionOption = assessmentQuestionOption;
+	}
+}
---
+++ service/tShell/src/main/java/com/cts/tshell/rest/SkillController.java
@@ -0,0 +1,37 @@
+package com.cts.tshell.rest;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.tshell.bean.Skill;
+import com.cts.tshell.service.SkillService;
+
+@RestController
+@RequestMapping("/skill")
+public class SkillController extends TshellController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SkillController.class);
+	@Autowired
+	private SkillService skillService;
+
+	@GetMapping("/top4searchedskills")
+	public List<Skill> getTop4SearchedSkills() {
+		LOGGER.info("start ");
+		List<Skill> top4SearchedSkills = skillService.getTop4Skills();
+		LOGGER.debug("Top 4 Searched Skills ->", top4SearchedSkills);
+		return top4SearchedSkills;
+	}
+
+	@GetMapping("/recentSkillList")
+	public List<Skill> getRecentSkills() {
+		LOGGER.info("start");
+		List<Skill> skills = skillService.getRecent5Skills();
+		LOGGER.debug("SkillController -> {}", skills);
+		return skills;
+	}
+}
