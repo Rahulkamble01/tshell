2c4507e79565ee6f5e66f784da78e2c24bcded3e
Kaja <vamsi.kaja@cognizant.com>
2019-01-03 10:13:00 +0530

    review method under construction

--- ui/tshell/src/app/contribute-question.service.ts
+++ ui/tshell/src/app/contribute-question.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs';
-
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ContributeQuestionService {
   uploadUrl: string = '/tShell/question/upload';
-  addQuestionUrl: string= "/tShell/";
+  addQuestionUrl: string = '/tShell/question/';
+  submitQuestionsUrl: string = '/tShell/question/submitforreview';
 
   constructor(private http: HttpClient) {
     this.http = http;
@@ -17,5 +22,10 @@ export class ContributeQuestionService {
   }
   addQuestion(json: any): Observable<any> {
     return this.http.post<any>(this.addQuestionUrl, json);
+  }
+  submitForReview(questionsList): Observable<any> {
+    console.log('submitForReview() is called!');
+    console.log(this.submitQuestionsUrl);
+    return this.http.post(this.submitQuestionsUrl, questionsList);
   }
 }
--- ui/tshell/src/app/preview-questions/preview-questions.component.ts
+++ ui/tshell/src/app/preview-questions/preview-questions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ContributeQuestionService } from '../contribute-question.service';
 
 @Component({
   selector: 'app-preview-questions',
@@ -7,16 +8,176 @@ import { Router } from '@angular/router';
   styleUrls: ['./preview-questions.component.css']
 })
 export class PreviewQuestionsComponent implements OnInit {
+  questionsList: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private contributeQuestionService: ContributeQuestionService) { }
 
   ngOnInit() {
   }
-  review(){
+  review() {
+    this.questionsList = [
+      {
+        "id": 0,
+        "question": " What allows the programmer to destroy an object x?",
+        "status": null,
+        "questionDifficultyLevel": null,
+        "questionAnswerType": null,
+        "createdUser": null,
+        "optionList": [
+          {
+            "id": 0,
+            "description": "x.delete()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "x.finalize()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "Runtime.getRuntime().gc()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "Only the garbage collection system can destroy an object.",
+            "question": null,
+            "answer": true,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": true
+          },
+          {
+            "id": 0,
+            "description": "x.garbage()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          }
+        ],
+        "empty": false,
+        "lengthExceeded": false
+      },
+      {
+        "id": 0,
+        "question": " What allows the programmer to destroy an object x?",
+        "status": null,
+        "questionDifficultyLevel": null,
+        "questionAnswerType": null,
+        "createdUser": null,
+        "optionList": [
+          {
+            "id": 0,
+            "description": "x.delete()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "x.finalize()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "Runtime.getRuntime().gc()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "Only the garbage collection system can destroy an object.",
+            "question": null,
+            "answer": true,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": true
+          },
+          {
+            "id": 0,
+            "description": "x.garbage()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          }
+        ],
+        "empty": false,
+        "lengthExceeded": false
+      },
+      {
+        "id": 0,
+        "question": " What allows the programmer to destroy an object x?",
+        "status": null,
+        "questionDifficultyLevel": null,
+        "questionAnswerType": null,
+        "createdUser": null,
+        "optionList": [
+          {
+            "id": 0,
+            "description": "x.delete()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "x.finalize()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "Runtime.getRuntime().gc()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          },
+          {
+            "id": 0,
+            "description": "Only the garbage collection system can destroy an object.",
+            "question": null,
+            "answer": true,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": true
+          },
+          {
+            "id": 0,
+            "description": "x.garbage()",
+            "question": null,
+            "answer": false,
+            "invalidAnswerFormat": false,
+            "lengthExceeded": false
+          }
+        ],
+        "empty": false,
+        "lengthExceeded": false
+      }
+    ];
     alert('Questions are submitted successfully for Review!');
+    this.contributeQuestionService.submitForReview(this.questionsList);
     this.router.navigate(['/contributeQuestion']);
   }
-  retry(){
+  retry() {
     alert('No Questions are posted!');
     this.router.navigate(['/contributeQuestion']);
   }
